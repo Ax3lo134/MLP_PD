3e4056a4774d49d7d8719bd640b92f65b356f82f
Negru Alexandru-George <negru.alexandru2000@gmail.com>
2022-04-05 15:53:10 +0300

    eng

--- main.py
+++ main.py
@@ -5,38 +5,38 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from glob import glob
 
-fisiere_date = glob('S*')
-pacient = pd.concat(pd.read_csv(file, sep=' ', names=["timp", "glezna oriz fata", "glezna vert", "glezna oriz lateral",
-                                                      "picior oriz fata", "picior vert", "picior oriz lateral",
-                                                      "sold oriz fata", "sold vert", "sold oriz lateral", "concluzii"])
-                    for file in fisiere_date)
-# 0 -> nu face parte din experiment
-# 1 -> fara sindrom
-# 2 -> cu sindrom 
+# data import
+files = glob('S*')
+pacient = pd.concat(pd.read_csv(file, sep=' ', names=["time",
+                                                      "forward ankle acceleration", "vert ankle acc", "ankle sides acc",
+                                                      "forward feet acc", "vert feet acc", "feet sides acc",
+                                                      "forward hip acc", "vert hip acc", "hip sides acc",
+                                                      "tags"])
+                    for file in files)
+# 0 -> unrelated
+# 1 -> no syndrome
+# 2 -> syndrome
 
-# pacient = pd.read_csv('S01R01.txt', sep=' ', names=["timp", "glezna oriz fata","glezna vert","glezna oriz lateral",
-# "picior oriz fata","picior vert","picior oriz lateral","sold oriz fata","sold vert","sold oriz lateral",
-# "concluzii"])
 
-
-# filtru pentru momentele de timp in care nu se desfasoara experimentul
+# filter for unrelated tags
 pacient = pacient[pacient.concluzii > 0]
 
-# am separat datele de intrare si datele de iesire.
-# intrare: timpul, senzorii; iesire: daca sindromul apare (2) sau daca nu apare sindrom (1)
-X = pacient.drop("concluzii", axis=1)
-y = pacient["concluzii"]
+# selects the input and output data
+# input (X) : time, acceleration sensors
+# output (Y) : tags
+X = pacient.drop("tags", axis=1)
+y = pacient["tags"]
 
-# definim vectorii in care vom pastra datele de invatare si cei pe care ii vom folosi sa testam modelul de A.I.
-# alegem cat la suta din datele pe care le avem la dispotitie sunt folosite pentru invatare
+# defines training and testing vectors
+# sets 25% of data for testing
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
-# folosim scalarea pentru a aduce toate variabilele la un nivel apropiat, pentru a nu exista diferente majore intre date
+# scales data to have an equal proportion
 scalare = StandardScaler()
 X_train = scalare.fit_transform(X_train)
 X_test = scalare.transform(X_test)
 
-# implementarea retelei neuronale
+# MLP implementation
 mlpc = MLPClassifier(hidden_layer_sizes=(10, 5), learning_rate_init=0.01)
 
 mlpc.fit(X_train, y_train)
